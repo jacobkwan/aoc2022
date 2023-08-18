635bf01723a37dcd339cc7164bff76e72eedde68
Jacob Kwan <77665182+jacobkwan@users.noreply.github.com>
2023-08-18 15:18:06 +0800

    Try FP approach for day3 part2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -2,8 +2,9 @@ use std::{collections::HashSet, fs::read_to_string};
 
 fn main() {
     let input = read_file("input.txt");
-    println!("{:?}", puzzle1(&input));
-    println!("{:?}", puzzle2(&input));
+    println!("Part 1: {:?}", puzzle1(&input));
+    println!("Part 2 (imperative): {:?}", puzzle2_imperative(&input));
+    println!("Part 2 (FP): {:?}", puzzle2_fp(&input));
 }
 
 fn puzzle1(input: &str) -> u32 {
@@ -30,7 +31,27 @@ fn priority(char: char) -> u32 {
     }
 }
 
-fn puzzle2(input: &str) -> u32 {
+fn puzzle2_fp(input: &str) -> u32 {
+    let input = input.lines().collect::<Vec<&str>>();
+    input
+        .chunks_exact(3) // every 3 lines is a group of elves
+        .map(|group| {
+            group
+                .iter()
+                .map(|rucksack| rucksack.chars().collect::<HashSet<char>>())
+                .reduce(|set_a, set_b| {
+                    set_a
+                        .intersection(&set_b)
+                        .cloned()
+                        .collect::<HashSet<char>>()
+                })
+                .map(|common_letters| common_letters.iter().next().unwrap().clone())
+                .map_or(0, priority)
+        })
+        .sum()
+}
+
+fn puzzle2_imperative(input: &str) -> u32 {
     let mut priorities_sum = 0;
     let num_groups = input.lines().count() / 3;
     for group_idx in 0..num_groups {
